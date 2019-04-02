20b994fbc45f61db4a2e94707181d58c7664ba0d
JasonASC4 <jasonhr781@gmail.com>
2019-04-02 08:32:03 -0400

    corrections

--- src/Card.java
+++ src/Card.java
@@ -1,47 +1,34 @@
 package Blackjack;
 
 class Card {
-/*
- * Creates a playing card.
- */
-private int rank;//represents the rank of a card
-private int suit;//represents the suit of a card
-private int value;//represents the value of a card
+
+private int rank;
+private int suit;
+private int value;
 private static String[] ranks = {"Joker","Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"};
 private static String[] suits = {"Clubs","Diamonds","Hearts","Spades"};
-/*
- * Created with an integer that represents a spot in the String array ranks and the String array suits. This represents
- * the rank and suit of an individual card.
- */
+
 Card(int suit, int values)
 {
     this.rank=values;
     this.suit=suit;
 }
-/*
- * Returns the string version of a card.
- */
+
 public String toString()
 {
     return ranks[rank]+" of "+suits[suit];
 }
-/*
- * Returns the rank of a card.
- */
+
 public int getRank()
 {
     return rank;
 }
-/*
- * Returns the suit of a card.
- */
+
 public int getSuit()
 {
     return suit;
 }
-/*
- * Returns the value of a card. If a jack, queen, or king the value is ten. Aces are 11 for now.
- */
+
 public int getValue()
 {
     if(rank>10)
@@ -58,9 +45,7 @@ public int getValue()
     }
     return value;
 }
-/*
- * Sets the value of a card.
- */
+
 public void setValue(int set)
 {
     value = set;
